c41baa51cc4c165f72e1c1d7f695872cf7849874
manikanthreddy6669 <manikanthreddy669@gmail.com>
2020-04-19 01:19:14 +0530

    First Commit

---
+++ SpringBoot-Admin/src/test/java/com/springbootAdmin/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springbootAdmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
